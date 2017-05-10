68b8cdca2d91f9208924c27b68d76920bea5f920
alinton12 <lintonarron@gmail.com>
2017-05-09 20:58:54 -0400

    added additions to array of data

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,10 +6,28 @@ app.controller('MainController', ['$scope', function($scope){
 
    $scope.promo = 'Inspirational';
   
-  $scope.product = {
-    name: "The Book of Trees",
-    price: 19,
-    pubdate: new Date('2014', '03', '08')
-
-  };
+	$scope.products = [ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg' 
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg' 
+  },
+  {
+    name: 'The Obstacle is the Way ',
+    price: 17,
+    pubdate: new Date('2014', '05', '01')
+  },
+  {
+    name: 'The 48 Laws of Power',
+    price: 19.99,
+    pubdate: new Date('1998', '08', '01')
+  }
+];
 }]);
