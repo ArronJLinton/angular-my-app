654391358f13ce98a41dd46ddee721270d3b92f9
alinton12 <lintonarron@gmail.com>
2017-05-09 23:11:39 -0400

    added voting feature

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,23 +11,34 @@ app.controller('MainController', ['$scope', function($scope){
     name: 'The Book of Trees', 
     price: 19, 
     pubdate: new Date('2014', '03', '08'), 
-    cover: 'img/the-book-of-trees.jpg' 
+    cover: 'img/the-book-of-trees.jpg',
+    likes: 0
   }, 
   { 
     name: 'Program or be Programmed', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg' 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0
   },
   {
     name: 'The Obstacle is the Way ',
     price: 17,
-    pubdate: new Date('2014', '05', '01')
+    pubdate: new Date('2014', '05', '01'),
+    likes: 0
   },
   {
     name: 'The 48 Laws of Power',
     price: 19.99,
-    pubdate: new Date('1998', '08', '01')
+    pubdate: new Date('1998', '08', '01'),
+    likes: 0
   }
 ];
+
+// The plusOne() function gets the index of the product that was clicked, and then adds one to that product's likes property.
+  $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
+
+
 }]);
